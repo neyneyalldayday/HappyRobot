f43bee4f7370f770966498a3e28e261d7def4dcb
neyneyalldayday <drfrankenstein123@gmail.com>
2021-08-20 12:17:24 -0500

    first commit on new site

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import "./layout.css"
+
+const Layout = ({ children }) => {
+    return (
+        <>
+          <main>{children}</main>  
+        </>
+    )
+}
+
+export default Layout
